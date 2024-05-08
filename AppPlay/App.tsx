a5a42da28e88049bf983c326125b8cc01d85b08a
Thiago Hansell <159553575+th2ol@users.noreply.github.com>
2024-05-08 11:51:49 -0300

    boa
    boa

---
+++ AppPlay/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default function App() {
+  return(
+    <View>
+      <Text>Hello Worddddd</Text>
+    </View>
+  );
+}
