33f607e681ad62d5468face9908141972a26ff4e
th2ol <159553575+th2ol@users.noreply.github.com>
2024-05-09 16:54:32 -0300

    casa
    casa

--- AppPlay/src/screens/index.tsx
+++ AppPlay/src/screens/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
 import { View, Text, TextInput } from "react-native";
 import { styles } from './styles'
+import React, { useState } from 'react';
 
 export function SingnIn() {
+  const [text, setText] = useState ("Você não digitou nada ainda");
+
   return(
     <View style={styles.container}>
       <TextInput style={styles.input} />
+
+      <Text>
+        Você digitou: {text}
+      </Text>
     </View>
   );
 }
