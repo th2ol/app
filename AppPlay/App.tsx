3ba797bf1ea8bddc8dc23f41a881dce4ff600066
Thiago Hansell <159553575+th2ol@users.noreply.github.com>
2024-05-10 11:55:41 -0300

    boa
    boa

--- AppPlay/App.tsx
+++ AppPlay/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SingnIn } from './src/screens/index';
+
 export default function App() {
   return(
    <SingnIn />
--- AppPlay/src/screens/index.tsx
+++ AppPlay/src/screens/index.tsx
@@ -1,19 +1,29 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, Image } from 'react-native';
 import { styles } from './styles'
 import React, { useState } from 'react';
+import IllustrationImg from '../assets/illustration.png';
+import { StatusBar } from 'expo-status-bar';
 
 export function SingnIn() {
   const [text, setText] = useState ("Você não digitou nada ainda");
 
   return(
     <View style={styles.container}>
-      <TextInput style={styles.input} 
-      onChangeText={setText}
-      />
+     <StatusBar barStyle='light-content' backgroundColor={"transparent"} translucent />
+      <Image source={IllustrationImg} />
+    <View style= {styles.contant}>
 
-      <Text>
-        Você digitou: {text}
+      <Text style={styles.title}>
+        Organize Suas {`/n`}
+
+
       </Text>
+      <Text style={styles.subtitle}>
+
+
+      </Text>
+
+    </View>
     </View>
   );
 }
